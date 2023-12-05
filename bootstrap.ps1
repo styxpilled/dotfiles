84172b943de9e0f6e2017b1476fc04dedd9ebd47
styxpilled <styxpilled.dev@gmail.com>
2023-12-05 03:38:54 +0100

    feat: remove powershell scripts

--- bootstrap.ps1
+++
@@ -1,9 +0,0 @@
-# Initialize path
-$profileDir = Split-Path -parent $profile
-
-# replace existing profile with dotfile profile for powershell and vscode powershell
-Copy-Item -Path "./profile.ps1" -Destination "$profileDir\Microsoft.PowerShell_profile.ps1"
-Copy-Item -Path "./profile.ps1" -Destination "$profileDir\Microsoft.VSCode_profile.ps1"
-
-# Clear unnecessary variables
-Remove-Variable profileDir
